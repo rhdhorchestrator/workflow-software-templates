700406edac9e01ef7963d7f6170ff8bbe020ae7f
Annel Ketcha <annelketcha@gmail.com>
2024-01-29 01:16:32 -0500

    reorganize default infrastructure workflow

--- template/assessment/skeleton/src/main/java/com/redhat/parodos/workflow/assessment/BasicAssessment.java
+++ template/assessment/skeleton/src/main/java/com/redhat/parodos/workflow/assessment/BasicAssessment.java
@@ -9,7 +9,7 @@ public class BasicAssessment {
     public WorkflowOptions execute(String userInput) {
         WorkflowOptions workflowOptions = new WorkflowOptions();
         if (userInput.toLowerCase().contains("default")) { // basic check on user input to mimic an assessment
-            workflowOptions.setCurrentVersion(new WorkflowOption("${{ values.workflowId }}DefaultInfrastructureWorkflow", "${{ values.description }} default infrastructure workflow"));
+            workflowOptions.setCurrentVersion(new WorkflowOption("${{ values.defaultInfrastructureWorkflowId }}", "${{ values.workflowId }} default infrastructure workflow"));
             workflowOptions.setUpgradeOptions(new ArrayList<>());
             workflowOptions.setMigrationOptions(new ArrayList<>());
             workflowOptions.setNewOptions(new ArrayList<>());
