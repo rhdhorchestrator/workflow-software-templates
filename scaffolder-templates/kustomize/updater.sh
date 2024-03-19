47e5be962f5fab0149a047622f571968d211efd6
Daniele Martinoli <86618610+dmartinol@users.noreply.github.com>
2024-03-19 08:20:27 +0100

    integrated kustomize project template

---
+++ scaffolder-templates/kustomize/updater.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+WORKFLOW_ID=$1
+IMAGE_TAG=$2
+folder="kustomize/base"
+
+files=$(find "$folder" -type f -name "0*.yaml" -print0 | sort -z | xargs -0 -n1 basename)
+yaml_entries=""
+for file in $files; do
+    yaml_entries+="  - $file\n"
+done
+
+# NOte: replace <sed -i ...>  with <sed -i '' ...> on MacOS
+sed -i "s|__RESOURCES__|$yaml_entries|" kustomize/base/kustomization.yaml
+sed -i "s|__IMAGE_TAG__|$IMAGE_TAG|" kustomize/base/kustomization.yaml
+
+cat kustomize/base/kustomization.yaml
