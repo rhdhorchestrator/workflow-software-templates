08a248f211ec793964e999852f2a7c80d9c419e0
Annel Ketcha <annelketcha@gmail.com>
2024-04-24 14:43:58 -0400

    enhance to avoid null values

--- scaffolder-templates/complex-assessment-workflow/skeleton/src/main/java/${{values.groupId}}/${{values.artifactId}}/DefaultAssessment.java
+++ scaffolder-templates/complex-assessment-workflow/skeleton/src/main/java/${{values.groupId}}/${{values.artifactId}}/DefaultAssessment.java
@@ -8,12 +8,7 @@ public class DefaultAssessment {
     public WorkflowOptions execute(String userInput) {
         WorkflowOptions workflowOptions = new WorkflowOptions();
         if (userInput.toLowerCase().contains("default")) { // basic check on user input to mimic an assessment
-            workflowOptions.setCurrentVersion(new WorkflowOption("${{ values.defaultInfrastructureWorkflowId }}", "${{ values.defaultInfrastructureWorkflowId }}"));
-            workflowOptions.setUpgradeOptions(new ArrayList<>());
-            workflowOptions.setMigrationOptions(new ArrayList<>());
-            workflowOptions.setNewOptions(new ArrayList<>());
-            workflowOptions.setContinuationOptions(new ArrayList<>());
-            workflowOptions.setOtherOptions(new ArrayList<>());
+            workflowOptions.setCurrentVersion(new WorkflowOption("${{ values.defaultInfrastructureWorkflowId }}", "${{ values.defaultInfrastructureWorkflowName }}"));
             return workflowOptions;
         }
         return workflowOptions;
